2b2b5e09c948752aceba5d55d2e86e90697a03fd
Francisco Perez <fperezfrancisco4@gmail.com>
2024-02-19 22:23:06 -0800

    finished cta form pages

--- ctaScript.js
+++ ctaScript.js
@@ -12,14 +12,34 @@ const formTitle1 = document.getElementById("formTitle-1");
 const formTitle2 = document.getElementById("formTitle-2");
 const formTitle3 = document.getElementById("formTitle-3");
 
+const submitFormBtn = document.getElementById("submitBtn-ctaForm");
+
+const ctaForm = document.getElementById("ctaForm");
+
 /** form validation */
 const playerFirstName = document.getElementById("playerFirstName");
 const playerLastName = document.getElementById("playerLastName");
 const playerDOB = document.getElementById("playerDOB");
+
+const parentName = document.getElementById("parentName");
+const relationshipRadio = document.querySelector(
+  "input[name='playerParent']:checked"
+);
+const playerEmail = document.getElementById("playerEmail");
+const playerPhone = document.getElementById("playerPhone");
+const playerAddress = document.getElementById("playerAddress");
+
+const currTeam = document.getElementById("currTeam");
+
 const fNameAlert = document.getElementById("fNameAlert");
 const lNameAlert = document.getElementById("lNameAlert");
 const dobAlert = document.getElementById("dobAlert");
 const genderAlert = document.getElementById("genderAlert");
+const contactNameAlert = document.getElementById("contactNameAlert");
+const relationshipAlert = document.getElementById("relationshipAlert");
+const emailAlert = document.getElementById("emailAlert");
+const currTeamAlert = document.getElementById("currTeamAlert");
+const levelAlert = document.getElementById("levelAlert");
 
 /** CTA form functionality */
 
@@ -37,14 +57,18 @@ nextBtnPage1.addEventListener("click", (e) => {
   } else if (playerDOB === null || playerDOB.value === "") {
     dobAlert.classList.add("active");
     return;
+  } else if (
+    document.querySelector("input[name='playerGender']:checked") === null ||
+    document.querySelector("input[name='playerGender']:checked").value === ""
+  ) {
+    console.log(document.querySelector("input[name='playerGender']:checked"));
+    genderAlert.classList.add("active");
+    return;
   } else {
     fNameAlert.classList.remove("active");
     lNameAlert.classList.remove("active");
     dobAlert.classList.remove("active");
-
-    console.log(
-      document.querySelector("input[name='playerGender']:checked").value
-    );
+    genderAlert.classList.remove("active");
     /** validate is over */
     firstPageForm.classList.remove("formPageActive");
     formNumber1.classList.remove("filled");
@@ -70,12 +94,30 @@ backPage2.addEventListener("click", (e) => {
 nextBtnPage2.addEventListener("click", (e) => {
   e.preventDefault();
   console.log("clicked on next btn page 2");
-  secondPageForm.classList.remove("formPageActive");
-  formNumber2.classList.remove("filled");
-  thirdPageForm.classList.add("formPageActive");
-  formNumber3.classList.add("filled");
-  formTitle2.classList.remove("active");
-  formTitle3.classList.add("active");
+  if (parentName === null || parentName.value === "") {
+    contactNameAlert.classList.add("active");
+    return;
+  } else if (
+    document.querySelector("input[name='playerParent']:checked") === null ||
+    document.querySelector("input[name='playerParent']:checked") === ""
+  ) {
+    relationshipAlert.classList.add("active");
+    return;
+  } else if (playerEmail === null || playerEmail.value === "") {
+    console.log("there is no email: " + playerEmail.value);
+    emailAlert.classList.add("active");
+    return;
+  } else {
+    contactNameAlert.classList.remove("active");
+    relationshipAlert.classList.remove("active");
+    emailAlert.classList.remove("active");
+    secondPageForm.classList.remove("formPageActive");
+    formNumber2.classList.remove("filled");
+    thirdPageForm.classList.add("formPageActive");
+    formNumber3.classList.add("filled");
+    formTitle2.classList.remove("active");
+    formTitle3.classList.add("active");
+  }
 });
 
 backPage3.addEventListener("click", (e) => {
@@ -87,4 +129,40 @@ backPage3.addEventListener("click", (e) => {
   formNumber2.classList.add("filled");
   formTitle2.classList.add("active");
   formTitle3.classList.remove("active");
+});
+
+submitFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("Clicked on final submit Btn");
+  /** check form before submitting */
+  console.log("Player First Name: " + playerFirstName.value);
+  console.log("Player Last Name: " + playerLastName.value);
+  console.log("Player DOB: " + playerDOB.value);
+  console.log(
+    "Player Gender: " +
+      document.querySelector("input[name='playerGender']:checked").value
+  );
+  console.log("Player Parent: " + parentName.value);
+  console.log(
+    "Player Relationship: " +
+      document.querySelector("input[name='playerParent']:checked").value
+  );
+  console.log("Player Email: " + playerEmail.value);
+  if (currTeam === null || currTeam.value === "") {
+    currTeamAlert.classList.add("active");
+    return;
+  } else if (
+    document.querySelector("input[name='playerLevel']:checked") === null ||
+    document.querySelector("input[name='playerLevel']:checked").value === ""
+  ) {
+    levelAlert.classList.add("active");
+    return;
+  } else {
+    console.log("Player Team: " + currTeam.value);
+    console.log(
+      "Curr Level: " +
+        document.querySelector("input[name='playerLevel']:checked").value
+    );
+    alert("Your form has been submitted! ");
+  }
 });
